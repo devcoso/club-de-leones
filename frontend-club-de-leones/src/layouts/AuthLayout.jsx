5498ad062314926e68b03fb7bc1f25e95f116eab
David Medina <cosoacount@gmail.com>
2024-11-11 12:00:13 -0600

    Creando login y modificando layout

--- frontend-club-de-leones/src/layouts/AuthLayout.jsx
+++ frontend-club-de-leones/src/layouts/AuthLayout.jsx
@@ -4,14 +4,14 @@ import logo from "../assets/logo.png"
 
 function AuthLayout() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center bg-gray-100">
-      <div className="w-full md:w-4/5 lg:w-2/3 h-full md:h-3/4 m-auto shadow-lg bg-white rounded-lg flex flex-col md:flex-row gap-2">
+    <div className="w-full md:h-screen flex flex-col justify-center bg-gray-100">
+      <div className="w-full md:w-4/5 lg:w-2/3 md:h-3/4 m-auto md:shadow-lg bg-white rounded-lg flex flex-col md:flex-row gap-2">
         <div className="md:w-1/2 m-auto">
           <Link to="/">
-            <img src={logo} alt="logo" className="m-auto" />
+            <img src={logo} alt="logo" className="m-auto w-72 md:w-auto" />
           </Link>
         </div>
-        <div className="md:w-1/2">
+        <div className="md:w-1/2 md:flex md:flex-col md:justify-center md:h-full">
           <Outlet />
         </div>
       </div>
--- frontend-club-de-leones/src/pages/auth/Login.jsx
+++ frontend-club-de-leones/src/pages/auth/Login.jsx
@@ -2,9 +2,10 @@ import { useState } from "react"
 
 import { InputText } from "primereact/inputtext"
 import { Password } from "primereact/password"
+import { Button } from "primereact/button"
 import { Divider } from "primereact/divider"
 
-import { Form } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
 
 function Login() {
 
@@ -14,18 +15,31 @@ function Login() {
   return (
     <div className="">
       <h1 className="text-primary font-bold text-center text-4xl my-2">Iniciar Sesión</h1>
-      <Form className="flex flex-col items-center" >
+      <Form className="flex flex-col items-center my-5 gap-6">
         <InputText 
           type="email" 
+          className="w-4/5"
           placeholder="Correo electrónico" 
         />
         <Password 
           placeholder="Contraseña"
           toggleMask
+          style={{width: '80%', display: 'block'}}
+          className="w-full"
+          inputClassName="w-full"
           feedback={false}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />  
+        <Button 
+          label="Iniciar Sesión" 
+          className="w-4/5 p-button-primary bg-primary"
+        />
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <Link to="/auth/forgot-password" className="text-primary">¿Olvidaste tu contraseña?</Link>
+          <Divider layout="vertical">o</Divider>
+          <Link to="/auth/register" className="text-primary">Regístrate</Link>
+        </div>
       </Form>
     </div>
   )
